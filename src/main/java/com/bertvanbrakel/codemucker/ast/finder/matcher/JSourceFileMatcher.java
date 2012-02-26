82ac7b179a63b97862450df294995a019c50d385
Bert van Brakel <bertvanbrakel@gmail.com>
2012-02-26 14:51:41 +0000

    removed codemucker files which were moved to their own module

--- src/main/java/com/bertvanbrakel/codemucker/ast/finder/matcher/JSourceFileMatcher.java
+++
@@ -1,9 +0,0 @@
-package com.bertvanbrakel.codemucker.ast.finder.matcher;
-
-import com.bertvanbrakel.codemucker.ast.JavaSourceFile;
-
-/**
- * Matcher to filter for {@link JavaSourceFile}'s
- */
-public interface JSourceFileMatcher extends Matcher<JavaSourceFile>{
-}
--- src/main/java/com/bertvanbrakel/codemucker/bean/BeanGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.bertvanbrakel.codemucker.bean;
-
-import com.bertvanbrakel.test.bean.BeanDefinition;
-
-public interface BeanGenerator {
-
-	public void generate(BeanDefinition def);
-}
